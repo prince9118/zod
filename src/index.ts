843315adb2745e44cf7d42e6694b8227fde14311
Prince <pchaudharyskn@gmail.com>
2026-05-13 23:38:13 +0530

    health-api

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,23 @@ const app= express();
 app.get('/health',(req,res)=>{
     const username= req.headers.username;
     const password= req.headers.password;
-    if(username==="Prince" && password==="9118"){
-        res.json({
-            success:true,
-            message:"your kideney  working"
-        
+    const kidneyId=Number(req.query.kidneyId);
+    if(!(username==="Prince" && password==="9118")){
+        res.status(400).json({
+            "msg":"something up with your inputs"
         });
     }
+    if( kidneyId != 1 && kidneyId != 2){
+        res.status(400).json({
+            "msg":"something up with your inputs"
+        });
+    }
+    // true
+    res.json({
+        success:true,
+        message:"your kideney  working"
+    
+    });
 });
 app.listen(3000,()=>{
     console.log("server is runing on port 3000");
